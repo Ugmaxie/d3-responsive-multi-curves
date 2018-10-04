63ca96e6b8cd6c0e9085298ac55dfef39600b683
trc <ugmaxie@gmail.com>
2018-10-04 19:28:45 +0300

    Add dual view mode with different design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OptionalComponent } from '../app/optional/optional.component';
+import { MainViewComponent } from '../app/mainView/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionalComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule
